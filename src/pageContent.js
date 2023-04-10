1fe27291462d155a389dd9721917db537dcd8b19
Justin Elliott <thecrashtestjustin@gmail.com>
2023-04-10 05:55:23 -0700

    moved helper function to general file

--- src/pageContent.js
+++ src/pageContent.js
@@ -1,14 +1,8 @@
+import { createDiv } from "./helperFunctions";
+
 export function header() {
   const mainSection = createDiv("header");
   mainSection.textContent = "Hello";
 
   return mainSection;
-}
-
-function createDiv(className, idName) {
-  const div = document.createElement("div");
-  div.className = className;
-  div.id = idName;
-
-  return div;
 }
