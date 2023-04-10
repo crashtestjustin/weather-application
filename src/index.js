9912e9e1dd2b860d8645ae6abda67c457647a185
Justin Elliott <thecrashtestjustin@gmail.com>
2023-04-10 07:14:52 -0700

    noting require data for later use

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import { header } from "./pageContent";
+import { header, body, footer } from "./pageContent";
 
 function application() {
   const app = document.querySelector(".application");
 
   const headerDiv = header();
   app.appendChild(headerDiv);
+
+  const bodyDiv = body();
+  app.appendChild(bodyDiv);
+
+  const footerDiv = footer();
+  app.appendChild(footerDiv);
 
   return app;
 }
--- src/pageContent.js
+++ src/pageContent.js
@@ -3,7 +3,32 @@ import { getForecastedWeather } from "./weatherApi";
 
 export function header() {
   const mainSection = createDiv("header");
-  mainSection.textContent = "Hello";
+  mainSection.textContent = "Header";
+
+  return mainSection;
+}
+
+export function body() {
+  const mainSection = createDiv("main");
+
+  const currentWeatherDiv = createDiv("current-weather");
+  currentWeatherDiv.textContent = `Current Weather`;
+  mainSection.appendChild(currentWeatherDiv);
+
+  const todayForecastedWeatherDiv = createDiv("forecasted-weather");
+  todayForecastedWeatherDiv.textContent = `Today Forecasted Weather`;
+  mainSection.appendChild(todayForecastedWeatherDiv);
+
+  const upcomingForecastedWeatherDiv = createDiv("upcoming-forecasted-weather");
+  upcomingForecastedWeatherDiv.textContent = `Upcoming Forecasted Weather`;
+  mainSection.appendChild(upcomingForecastedWeatherDiv);
+
+  return mainSection;
+}
+
+export function footer() {
+  const mainSection = createDiv("footer");
+  mainSection.textContent = "Footer";
 
   return mainSection;
 }
