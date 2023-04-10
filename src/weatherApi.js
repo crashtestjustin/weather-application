3717b2f0ff9b82c7c5f28f9e1b88244fb52a8015
Justin Elliott <thecrashtestjustin@gmail.com>
2023-04-10 07:52:52 -0700

    initial modifier functions to obtain weather data

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,15 +1,29 @@
 export async function getForecastedWeather(locationQuery) {
   let baseUrl =
-    "http://api.weatherapi.com/v1/forecast.json?key=4e0461a2596747cfb3e125853230404&days=14&q=";
+    "http://api.weatherapi.com/v1/forecast.json?key=4e0461a2596747cfb3e125853230404&days=3&q=";
   let searchUrl = `${baseUrl}${locationQuery}`;
   console.log(searchUrl);
   try {
     const response = await fetch(searchUrl, { mode: "cors" });
     const weatherData = await response.json();
-
     //set locations on page with weather data as needed or export to set on another module
-    console.log(weatherData);
+    currentWeatherStats(weatherData.current, weatherData.location);
+    futureWeather(weatherData.forecast);
   } catch (error) {
     console.log(error);
   }
+}
+
+function currentWeatherStats(currentWeather, locationData) {
+  console.log(currentWeather.condition.text);
+  console.log(currentWeather.temp_c);
+  console.log(currentWeather.feelslike_c);
+  console.log(currentWeather.temp_f);
+  console.log(currentWeather.feelslike_f);
+  console.log(locationData.name);
+  console.log(locationData.localtime);
+}
+
+function futureWeather(futureWeatherData) {
+  console.log(futureWeatherData);
 }
