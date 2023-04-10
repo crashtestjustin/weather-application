3a55428f0dd0d1fb7bd3207ec8b2af247929c7e1
Justin Elliott <thecrashtestjustin@gmail.com>
2023-04-10 07:14:35 -0700

    updated createDiv helper function output

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -2,6 +2,6 @@ export function createDiv(className, idName) {
   const div = document.createElement("div");
   div.className = className;
   div.id = idName;
-
+  div.id === "undefined" ? (div.id = className) : "";
   return div;
 }
