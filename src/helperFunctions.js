eb2df5742920375c369912b8827e63b8de76cd6c
Justin Elliott <thecrashtestjustin@gmail.com>
2023-04-11 06:22:51 -0700

    created new helper functions for basic UI elements

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -5,3 +5,19 @@ export function createDiv(className, idName) {
   div.id === "undefined" ? (div.id = className) : "";
   return div;
 }
+
+export function createInput(inputType, inputId, inputName, inputPlaceholder) {
+  const input = document.createElement("input");
+  input.type = inputType;
+  input.id = inputId;
+  inputName = inputName;
+  input.placeholder = inputPlaceholder;
+  return input;
+}
+
+export function createButton(btnClass, btnTextContent) {
+  const button = document.createElement("button");
+  button.className = btnClass;
+  button.textContent = btnTextContent;
+  return button;
+}
